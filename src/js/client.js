7ba690959a5e9caca666981def8fc2eefa4948ef
Nadeem Shakya <shakya.nadim12@gmail.com>
2018-09-06 08:51:41 +0545

    Update client.js

--- src/js/client.js
+++ src/js/client.js
@@ -1,1 +1,13 @@
 
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+
+export default class Home extends Component {
+  render() {
+    return(
+      <h1>Welcome to React Router Tutorial </h1>
+    );
+  }
+}
+const app = document.getElementById('app');
+ReactDOM.render(<Home />, app);
