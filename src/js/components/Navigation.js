3f8a782c1ed27a1e81bc1c38176c4c2d30efc590
Nadeem Shakya <shakya.nadim12@gmail.com>
2018-09-07 12:48:28 +0545

    Create Navigation.js

---
+++ src/js/components/Navigation.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+export default class Navigation extends Component {
+    render() {
+        return(
+            <div className = 'navigationDiv'>
+              {/* We will add our navigation links here. */}
+            </div>
+        );
+    }
+}
