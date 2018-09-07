6d7fe6c54093f54c5d286d39cd7c0067b702c8c6
Nadeem Shakya <shakya.nadim12@gmail.com>
2018-09-07 12:42:07 +0545

    Add files via upload

---
+++ src/js/components/Home.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class Home extends Component {
+    render() {
+        return(
+            <div>
+                <h3>Home</h3>
+                <span>Welcome, this is the  home section.</span>
+            </div>
+        );
+    }
+}
