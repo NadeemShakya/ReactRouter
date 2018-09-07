b31f85492f1a31b7c9ad937d96db1897cca06aaf
Nadeem Shakya <shakya.nadim12@gmail.com>
2018-09-07 13:26:08 +0545

    Update Navigation.js

--- src/js/components/Navigation.js
+++ src/js/components/Navigation.js
@@ -1,9 +1,15 @@
 import React,{Component} from 'react';
+import {NavLink} from 'react-router-dom';
+
 export default class Navigation extends Component {
     render() {
         return(
-            <div className = 'navigationDiv'>
-              {/* We will add our navigation links here. */}
+            <div className = 'navigationDiv'>                
+            <ul>
+                <li><NavLink exact to = '/'>Home</NavLink></li>
+                <li><NavLink to = '/archives'>Archives</NavLink></li>
+                <li><NavLink to = '/settings'>Settings</NavLink></li>
+             </ul>
             </div>
         );
     }
